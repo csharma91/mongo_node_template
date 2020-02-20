83ac3e52c08f3110c5fad1261173cac04bb8e6b8
Chintan Sharma <chintan@Chintans-MBP.hitronhub.home>
2020-02-19 23:40:17 -0700

    changes

---
+++ client-react/src/components/pages/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Home Page
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default home
---
+++ client-react/src/components/pages/signup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class signup extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Sign Up
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default signup
